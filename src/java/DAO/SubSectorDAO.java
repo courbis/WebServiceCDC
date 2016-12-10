872cb40a565ee5a0c5e10c76ebdce2edc1ec907c
Julio Courbis <ju.courbis@gmail.com>
2016-12-10 14:41:26 -0300

    v2

--- src/java/DAO/SubSectorDAO.java
+++ src/java/DAO/SubSectorDAO.java
@@ -13,11 +13,11 @@ public class SubSectorDAO {
     
     public LinkedList<SubSector> obtenerSubSector(int idSector) throws SQLException{
         LinkedList<SubSector> listaSubSector=new LinkedList<SubSector>();
-    String query="select id_subsector, tipo_subsector.detalle+''+subsector.detalle as subsector "
-            + "from obra,sector,tipo_sector,subsector,tipo_subsector,sector_obra "
-            + "where 3 and "
-            + "sector.id_tipo_sector=tipo_sector.id_tipo_sector and subsector.id_obra_sector=sector_obra.id_sector_obra and "
-            + "tipo_subsector.id_tipo_subsector=subsector.id_tipo_subsector and sector.id_sector="+idSector+";";
+    String query="select id_subsector, tipo_subsector.detalle+' '+subsector.detalle as subsector "
+       + "from obra,sector,tipo_sector,subsector,tipo_subsector,sector_obra "
+             + "where sector_obra.id_sector=sector.id_sector and sector_obra.id_obra=obra.id_obra and "
+             + "sector.id_tipo_sector=tipo_sector.id_tipo_sector and subsector.id_obra_sector=sector_obra.id_sector_obra and "
+             + "tipo_subsector.id_tipo_subsector=subsector.id_tipo_subsector and sector.id_sector="+idSector+";";
         Connection connection=Conexion.conectarBD();
         Statement statement= connection.createStatement();
         ResultSet resultSet= statement.executeQuery(query);  
